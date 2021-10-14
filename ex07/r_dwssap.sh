e94fde3636b6b8d614f91573b9203fea8aac300c
Jinust <hyeongjinh@naver.com>
2021-10-14 15:59:01 +0900

    1

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd |grep -v "#" |awk "(NR+1) %2"
+cat /etc/passwd | grep -v "#" |awk "(NR+1)%2" |cut -d "*" -f1 |rev |sort -r |awk 'NR>=7 && NR<=10'|tr -d ":"| tr "\n" ","|sed 's/.$//'|sed 's/$/./'|tr -d "\n"
